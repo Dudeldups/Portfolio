9956ab98ed62f15e9fe6e36b34daf83be36adefb
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-01-21 19:24:44 +0100

    added h1 card

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -6,27 +6,31 @@ export default function CTA() {
   return (
     <main className="cta">
       <section id="about" className="hero">
-        <h1 className="hero__title title--main">
-          Crafting Tomorrow's Web:
-          <span>Learn.</span>
-          <span>Apply.</span>
-          <span>Improve.</span>
-          <span>Repeat.</span>
-        </h1>
+        <div className="hero__wrapper">
+          <div className="hero__title-card">
+            <h1 className="hero__title title--main">
+              Crafting Tomorrow's Web:
+              <span className="title--word">Learn.</span>
+              <span className="title--word">Apply.</span>
+              <span className="title--word">Improve.</span>
+              <span className="title--word">Repeat.</span>
+            </h1>
+          </div>
 
-        <div className="hero__text-card">
-          <h2 className="hero__title title--sub">About me</h2>
-          <p className="hero__desc">
-            I'm Arne, a Frontend Web Developer based near Dortmund, Germany. My
-            passion lies in creating easy-to-use, appealing and accessible
-            websites that leave a lasting impression. Let's build something
-            extraordinary together!
-          </p>
+          <div className="hero__text-card">
+            <h2 className="hero__title title--sub">About me</h2>
+            <p className="hero__desc">
+              I'm Arne, a Frontend Web Developer based near Dortmund, Germany.
+              My passion lies in creating easy-to-use, appealing and accessible
+              websites that leave a lasting impression. Let's build something
+              extraordinary together!
+            </p>
+          </div>
+
+          <a className="btn btn--contact" href="#contact">
+            Contact me
+          </a>
         </div>
-
-        <a className="btn btn--contact" href="#contact">
-          Contact me
-        </a>
       </section>
 
       <Projects />
